e3ed48b6e8e231f53cc42950adaf24bdb60c466d
AnnaZaluzhnaya <92920694+AnnaZaluzhnaya@users.noreply.github.com>
2022-07-05 14:24:13 +0200

    uppdate Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,13 @@
 import styles from './Profile.module.css';
 import PropTypes from 'prop-types';
 
-const Profile = ({ username, tag, location, avatar, stats }) => {
+const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
   return (
     <div className={styles.profile}>
       <DescriptionContainer
@@ -11,20 +17,7 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
         avatar={avatar}
       />
 
-      <ul className={styles.stats}>
-        <li className={styles.statsItem}>
-          <span className={styles.label}>Followers</span>
-          <span className={styles.quantity}>{stats.followers}</span>
-        </li>
-        <li className={styles.statsItem}>
-          <span className={styles.label}>Views</span>
-          <span className={styles.quantity}>{stats.views}</span>
-        </li>
-        <li className={styles.statsItem}>
-          <span className={styles.label}>Likes</span>
-          <span className={styles.quantity}>{stats.likes}</span>
-        </li>
-      </ul>
+      <StatsContainer followers={followers} views={views} likes={likes} />
     </div>
   );
 };
@@ -37,6 +30,25 @@ export function DescriptionContainer({ avatar, location, tag, username }) {
       <p className={styles.tag}>@{tag}</p>
       <p className={styles.location}>{location}</p>
     </div>
+  );
+}
+
+export function StatsContainer({ followers, views, likes }) {
+  return (
+    <ul className={styles.stats}>
+      <li className={styles.statsItem}>
+        <span className={styles.label}>Followers</span>
+        <span className={styles.quantity}>{followers}</span>
+      </li>
+      <li className={styles.statsItem}>
+        <span className={styles.label}>Views</span>
+        <span className={styles.quantity}>{views}</span>
+      </li>
+      <li className={styles.statsItem}>
+        <span className={styles.label}>Likes</span>
+        <span className={styles.quantity}>{likes}</span>
+      </li>
+    </ul>
   );
 }
 
